80a6aeae112667a95a53e2a8d61532e565a1d07c
David Perez <david@closemarketing.es>
2022-04-07 10:20:49 +0200

    loader

--- includes/connect-woocommerce/loader.php
+++ includes/connect-woocommerce/loader.php
@@ -10,18 +10,28 @@
 
 defined( 'ABSPATH' ) || exit;
 
-require_once dirname( __FILE__ ) . '/includes/connect-woocommerce/helpers-functions.php';
+require_once dirname( __FILE__ ) . '/lib/helpers-functions.php';
+
+// Loads translation.
+add_action( 'init', 'wcsen_load_textdomain' );
+/**
+ * Load plugin textdomain.
+ */
+function wcsen_load_textdomain() {
+	load_plugin_textdomain( 'connect-woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+
 // Creates table sync.
 if ( connwoo_is_pro() ) {
 	register_activation_hook( __FILE__, 'connwoo_process_activation_premium' );
 }
 
 // Includes files.
-require_once dirname( __FILE__ ) . '/includes/class-api-erp-neo.php';
-require_once dirname( __FILE__ ) . '/includes/connect-woocommerce/helpers-cron.php';
-require_once dirname( __FILE__ ) . '/includes/connect-woocommerce/class-connect-admin.php';
-require_once dirname( __FILE__ ) . '/includes/connect-woocommerce/class-connect-import.php';
-require_once dirname( __FILE__ ) . '/includes/connect-woocommerce/class-connect-import-pro.php';
+require_once dirname( __FILE__ ) . '/lib/class-api-erp-neo.php';
+require_once dirname( __FILE__ ) . '/lib/helpers-cron.php';
+require_once dirname( __FILE__ ) . '/lib/class-connect-admin.php';
+require_once dirname( __FILE__ ) . '/lib/class-connect-import.php';
+require_once dirname( __FILE__ ) . '/lib/class-connect-import-pro.php';
 
 // Orders sync.
-require_once dirname( __FILE__ ) . '/includes/connect-woocommerce/class-connect-orders.php';
+require_once dirname( __FILE__ ) . '/lib/class-connect-orders.php';
