f1fe6fd06f130909c5c9156ddb173ef3fd17aac3
davidperezgar <david@closemarketing.es>
2023-07-07 00:02:33 +0200

    prevents fatal error syncing

--- lib/class-connect-import.php
+++ lib/class-connect-import.php
@@ -456,7 +456,7 @@ if ( ! class_exists( 'Connect_WooCommerce_Import' ) ) {
 			if ( ! session_id() ) {
 				session_start();
 			}
-			if ( 0 === $sync_loop ) {
+			if ( 0 === $sync_loop || empty( $_SESSION['api_products'] ) ) {
 				$api_products             = $this->connapi_erp->get_products();
 				$_SESSION['api_products'] = $api_products;
 			} else {
